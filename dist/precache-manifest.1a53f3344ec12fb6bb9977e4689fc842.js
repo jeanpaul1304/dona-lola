4b4c95fd3d1c35e99f8032da8a2eeb8f2cc21b53
Jean Diaz <jeanpaul1304@gmail.com>
2018-10-28 14:21:54 -0500

    dist

---
+++ dist/precache-manifest.1a53f3344ec12fb6bb9977e4689fc842.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dona-lola/dist/robots.txt"
+  },
+  {
+    "revision": "8394e5dbf1a7a48d4ccf",
+    "url": "/dona-lola/dist/js/chunk-vendors.fe6448b9.js"
+  },
+  {
+    "revision": "6d8d4ab375dbc82d5477",
+    "url": "/dona-lola/dist/js/app.a5232482.js"
+  },
+  {
+    "revision": "63252155dca5695b5b8c",
+    "url": "/dona-lola/dist/js/about.6fe3c8de.js"
+  },
+  {
+    "revision": "9517b6088ffb197d8119cbc220ff3430",
+    "url": "/dona-lola/dist/index.html"
+  },
+  {
+    "revision": "b7f4582799181f0e4d41a996a991f25d",
+    "url": "/dona-lola/dist/img/logo.b7f45827.png"
+  },
+  {
+    "revision": "662c51cc73d3463b72b62f6d6a185ad1",
+    "url": "/dona-lola/dist/img/bg.662c51cc.jpg"
+  },
+  {
+    "revision": "8394e5dbf1a7a48d4ccf",
+    "url": "/dona-lola/dist/css/chunk-vendors.5cf92f0a.css"
+  },
+  {
+    "revision": "6d8d4ab375dbc82d5477",
+    "url": "/dona-lola/dist/css/app.02f55dc3.css"
+  }
+];
