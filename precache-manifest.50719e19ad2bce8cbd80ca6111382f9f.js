af10c553aaabee55658e357ce98eba04563f6fc9
Jean Diaz <jeanpaul1304@gmail.com>
2018-10-27 23:49:18 -0500

    first commit

---
+++ precache-manifest.50719e19ad2bce8cbd80ca6111382f9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8394e5dbf1a7a48d4ccf",
+    "url": "/js/chunk-vendors.fe6448b9.js"
+  },
+  {
+    "revision": "d87055354731431b9859",
+    "url": "/js/app.1e8a2742.js"
+  },
+  {
+    "revision": "63252155dca5695b5b8c",
+    "url": "/js/about.6fe3c8de.js"
+  },
+  {
+    "revision": "b39fc1715033a958cbe128d6d3032c66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f4582799181f0e4d41a996a991f25d",
+    "url": "/img/logo.b7f45827.png"
+  },
+  {
+    "revision": "662c51cc73d3463b72b62f6d6a185ad1",
+    "url": "/img/bg.662c51cc.jpg"
+  },
+  {
+    "revision": "8394e5dbf1a7a48d4ccf",
+    "url": "/css/chunk-vendors.5cf92f0a.css"
+  },
+  {
+    "revision": "d87055354731431b9859",
+    "url": "/css/app.02f55dc3.css"
+  }
+];
